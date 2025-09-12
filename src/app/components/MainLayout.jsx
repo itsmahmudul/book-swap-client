cf48028eedcca3a239cbf1457a05b4ac0a8427ad
akrmSujan <sujanban223@gmail.com>
2025-09-13 02:34:28 +0600

    Components Folder created and mainlayout created to wrap the home layout and also to separate admin dashboard.

---
+++ src/app/components/MainLayout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Footer from './Footer'
+
+export default function MainLayout({children}) {
+  return(
+    <>
+ <nav><Navbar/></nav>
+ <main>{children}</main>
+ <footer><Footer/></footer>
+  </>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,8 @@
-import Image from "next/image";
+import MainLayout from "./components/MainLayout";
 
 export default function Home() {
-  return (
-    <div>
-      <h1 className="text-8xl text-red-700">
-        Hi Guys This is team loading doing a team project
-      </h1>
-    </div>
+  return (  
+    <MainLayout>
+    </MainLayout>
   );
 }
