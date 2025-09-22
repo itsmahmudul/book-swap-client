33c940bd80129e53f17e6f2af7f3e14a7c5ce730
akrmSujan <sujanban223@gmail.com>
2025-09-22 19:57:39 +0600

    nvabar fixed

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,10 @@
-
 import Navbar from "./components/Navbar";
-import { ThemeToggle } from "./themeToggle";
 
 export default function Home() {
   return (  
   <>
   <nav><Navbar></Navbar></nav>
-  <ThemeToggle/>
+
   </>
   );
 }
